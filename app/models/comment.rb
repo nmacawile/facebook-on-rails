72572d8bf09de398e359972898735afeeb762bfa
Mac <nmacawile@gmail.com>
2018-03-11 12:27:49 +0000

    Change default comment ordering

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,7 @@ class Comment < ApplicationRecord
   has_many :likes, foreign_key: :likeable_id
   has_many :likers, through: :likes
   
+  default_scope { order(:id) }
+  
   validates :body, length: { maximum: 255 }
 end
