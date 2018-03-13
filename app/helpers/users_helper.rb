646d52a96c1c5b3dd3cd86291d43480d27161e30
Mac <nmacawile@gmail.com>
2018-03-13 10:56:55 +0000

    Add styles to friending buttons

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,17 +1,43 @@
 module UsersHelper
   
   def friendship_action_links(user)
-    if user != current_user
-      if friendship = Friendship.fetch(current_user, user)
-        link_to "Remove as Friend", friendship_path(friendship), method: :delete, data: { confirm: "Are you sure?" } 
-      elsif friend_request_out = FriendRequest.fetch(current_user, user)
-        link_to "Cancel Friend Request", friend_request_path(friend_request_out), method: :delete, data: { confirm: "Are you sure?" } 
-      elsif friend_request_in = FriendRequest.fetch(user, current_user)
-        link_to("Confirm as Friend", friendships_path(id: user.id), method: :post) << " ".html_safe << 
-        link_to("Reject Request", friend_request_path(friend_request_in), method: :delete, data: { confirm: "Are you sure?" })
-      else
-        link_to "Add as Friend", friend_requests_path(id: user.id), method: :post
-      end
+    return unless user_signed_in? && user != current_user
+  
+    if friendship = Friendship.fetch(current_user, user)
+      link_with_icon(["fas", "user-times"],
+                     "Remove as Friend",
+                     friendship_path(friendship),
+                       method: :delete,
+                       data: { confirm: "Are you sure?" },
+                       class: ["btn", "btn-danger", "btn-long"])
+    elsif friend_request_out = FriendRequest.fetch(current_user, user)
+      link_with_icon(["fas", "user-times"],
+                      "Cancel Request",
+                      friend_request_path(friend_request_out),
+                        method: :delete,
+                        data: { confirm: "Are you sure?" },
+                        class: ["btn", "btn-warning", "btn-long"])
+    elsif friend_request_in = FriendRequest.fetch(user, current_user)
+      content_tag(:div,
+                  link_with_icon(["fas", "check"],
+                                 "Confirm",
+                                 friendships_path(id: user.id),
+                                   method: :post,
+                                   class: ["btn", "btn-success", "btn-short"]) << 
+                  link_with_icon(["fas", "times"],
+                                  "Reject",
+                                  friend_request_path(friend_request_in),
+                                    method: :delete,
+                                    data: { confirm: "Are you sure?" },
+                                    class: ["btn", "btn-danger", "btn-short"]),
+                  class: "btn-group")
+    else
+      link_with_icon(["fas", "user-plus"],
+                     "Add as Friend",
+                     friend_requests_path(id: user.id),
+                       method: :post,
+                       class: ["btn", "btn-primary", "btn-long"])
+  
     end
   end
 end
