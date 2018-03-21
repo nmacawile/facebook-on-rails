3d9a7fbe2e2a25f87a787cc8a5f7948725cdbc08
Mac <nmacawile@gmail.com>
2018-03-21 18:21:56 +0000

    Allow line breaks on posts

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Workspace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.action_view.sanitized_allowed_tags = ['a']
+    config.action_view.sanitized_allowed_tags = ['a', 'br']
     config.action_view.sanitized_allowed_attributes = ['href', 'title']
   end
 end
